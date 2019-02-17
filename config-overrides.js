622cc7294fb272904147f05d03fcd33953232a79
Ovidiu Alexandrescu <owystyle@gmail.com>
2019-02-17 20:17:05 +0200

    Initial commit

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const { override, babelInclude } = require("customize-cra");
+const path = require("path");
+
+module.exports = override(
+  babelInclude([
+    path.resolve("src"), // make sure you link your own source
+    // path.resolve("node_modules/your-module")
+  ])
+);
